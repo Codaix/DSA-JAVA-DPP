b251c3703f3686a34bb76f7c3d378cae37fe2367
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-10 21:15:38 +0530

    Q1

--- Q1.java
+++ Q1.java
@@ -1,5 +1,16 @@
 package DSA_JAVA_DPP;
-
+//// Homework: return the last occurance of a character in a sequence.
+    
 public class Q1 {
+    public static void main(String[] args) {
+        
+    }
+    int returnLastCharRec(String str, char c, int i){
+        //here i = str.length()-1;
+        if(i < 0||i>= str.length())return -1;
+        if(str.charAt(i) == c)return i;
+        int index = returnLastCharRec(str, c, i-1);
+        return index;
+    }
     
 }
