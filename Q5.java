655f426b668d11805b42fcb6a3b7a31de39dc13c
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-13 11:57:34 +0530

    Q5.. pending..

---
+++ Q5.java
@@ -0,0 +1,7 @@
+/*
+Que: we are supposed to created a matrix class inside which we are having a matrix. 
+    it provides methods as add, multiply and display.we need to create two matrix objects.
+    and perform add and display and similary we need to multiply and call the display method.
+*/
+public class Q5 {
+}
