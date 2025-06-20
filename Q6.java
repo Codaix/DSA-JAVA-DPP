d5eef3354614ba5145df917541f225de48b84654
Codaix <160418176+Codaix@users.noreply.github.com>
2025-06-20 22:02:21 +0530

    Q6

---
+++ Q6.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+class query{
+    void nums(int a, int b, int n){
+        int result = a;
+        for(int i = 0; i<n; i++){
+            result += (int)(Math.pow(2,i)*b);
+            System.out.print(result+" ");
+        }
+        System.out.println();
+    }
+    
+}
+public class Q6{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt();
+        query  obj= new query();
+        
+        for(int i=0;i<q;i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+            obj.nums(a,b,n);
+        }
+        sc.close();
+    }
+}
